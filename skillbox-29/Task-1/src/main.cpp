afa7ed342779287805d376dfe7f1753209912a7f
Maxim Mikhailov <mos_gor@list.ru>
2023-10-10 13:12:32 +0300

    Redid task 1 from lesson 29

--- skillbox-29/Task-1/src/main.cpp
+++ skillbox-29/Task-1/src/main.cpp
@@ -3,60 +3,75 @@
 #include <vector>
 
 class Talent{
-protected:
-    std::vector<std::string> talents;
-    virtual void add_talent() = 0;
+public:
+    virtual std::string show_talents() = 0;
 };
 
 class Swimming: virtual public Talent{
-    void add_talent(){
-        talents.push_back("Swim");
-    }
 public:
-    Swimming(){
-        this->add_talent();
+    std::string show_talents(){
+        return "Swim";
     }
 };
 
 class Counting: virtual public Talent{
-    virtual void add_talent(){
-        talents.push_back("Count");
-    }
 public:
-    Counting(){
-        this->add_talent();
+    std::string show_talents(){
+        return "Count";
     }
 };
 
 class Dancing: virtual public Talent{
-    virtual void add_talent(){
-        talents.push_back("Dance");
-    }
 public:
-    Dancing(){
-        this->add_talent();
+    std::string show_talents(){
+        return "Dance";
     }
 };
 
-class Dog: public Counting, public Dancing, public Swimming{
+class Dog{
     std::string name;
-    void add_talent(){};
+    std::vector<Talent*> talents;
 public:
     void show_talents(){
-        std::cout << "This is " << name << " and is has some talents:\n";
-        for (int i = 0; i < talents.size(); i++){
-            std::cout << "It can \"" << talents[i] << "\"\n";
+        std::cout << "This is " << name;
+        if (!talents.empty()){
+            std::cout << " and it has some talents:\n";
+            for (int i = 0; i < talents.size(); i++){
+                std::cout << "It can \"" << talents[i]->show_talents() << "\"\n";
+            }
+        }
+        else{
+            std::cout << " and it has no talents\n";
         }
     }
 
+    void add_talent(Talent* someTalent){
+        talents.push_back(someTalent);
+    }
+
     Dog(std::string inName): name (inName){}; 
+
+    ~Dog(){
+        for (int i = 0; i < talents.size(); i++){
+            delete talents[i];
+        }
+    }
 };
 
 int main(){
     std::cout << "Input dog's name: ";
-    std::string name;
+    std::string name, temp;
     std::cin >> name;
     Dog d(name);
+    std::cout << "Can it swim?\n";
+    std::cin >> temp;
+    if (temp == "Yes" || temp == "y" || temp == "yes"){d.add_talent(new Swimming);}
+    std::cout << "Can it dance?\n";
+    std::cin >> temp;
+    if (temp == "Yes" || temp == "y" || temp == "yes"){d.add_talent(new Dancing);}
+    std::cout << "Can it count?\n";
+    std::cin >> temp;
+    if (temp == "Yes" || temp == "y" || temp == "yes"){d.add_talent(new Counting);}
     d.show_talents();
     return 0;
 }
